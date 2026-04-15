5c22fc5860bd999592cc91e68cc69f94387541f9
HeudreXeN <143484352+HeudreXeN@users.noreply.github.com>
2026-04-15 18:38:21 +0200

    Update server.js

--- server.js
+++ server.js
@@ -7,6 +7,9 @@ const { Pool } = require("pg");
 
 const db = new Pool({
     connectionString: process.env.DATABASE_URL
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 ///////////////////////////////////////////////////////////
 const bcrypt = require("bcrypt");
